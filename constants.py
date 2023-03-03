6bee8639a849d06dd25ff37a8b149fad19410749
Mario4009 <83692931+Mario4009@users.noreply.github.com>
2023-03-03 14:44:03 +0100

    Add vision input

--- constants.py
+++ constants.py
@@ -1,1 +1,3 @@
 BROKER_IP_ADDRESS = '192.168.1.111'
+VISION_OUTPUT_FILENAME = 'C:/Users/mario/Documents/makeathon/test.json'
+LOOP_TIMEOUT = 0.5
--- main.py
+++ main.py
@@ -1,25 +1,27 @@
-from constants import BROKER_IP_ADDRESS
+from gc import garbage
+import time
+from constants import BROKER_IP_ADDRESS, LOOP_TIMEOUT, VISION_OUTPUT_FILENAME
 from lib.mqtt import MQTTClient
 from lib.payload import Boat, Garbage
+from lib.vision import read_output
 
 
-def send_vision_data(mqtt_client: MQTTClient):
-    boat = Boat(
-        position_x_px=500,
-        position_y_px=200,
-        direction_x=1,
-        direction_y=-1,
-        angle_boat_garbage_deg=24,
-        speed_boat_m_per_s=0.02
-    )
 
+def send_vision_data(mqtt_client: MQTTClient, garbage: Garbage | None, boat: Boat):
     boat.publish(mqtt_client)
+    if garbage is not None:
+        garbage.publish(mqtt_client)
 
 
 def main():
     mqtt_client = MQTTClient()
     mqtt_client.connect(BROKER_IP_ADDRESS)
-    send_vision_data(mqtt_client)
+    while True:
+        vision_data = read_output(VISION_OUTPUT_FILENAME)
+        if vision_data is not None:
+            garbage, boat = vision_data
+            send_vision_data(mqtt_client, garbage, boat)
+        time.sleep(LOOP_TIMEOUT)
 
 if __name__ == "__main__":
     main()
